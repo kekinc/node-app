8acd060b205d58b4529c359ae09a028f5ef5a12a
Kekin Chheda <kekincchheda@gmail.com>
2020-08-03 00:37:43 +0530

    Changes in test cases

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ request = require('supertest');
 
 describe('GET /', ()=>{
     it('should respond with Hello to node-express world!',(done)=>{
-        request(app).get('/').expect('Hello to node-express world!!'); 
+        request(app).get('/').expect('Hello! This is massive change.'); 
         done();
     });
 });
