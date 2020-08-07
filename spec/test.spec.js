36310818e909a95f461d552fac82771677b3160c
Kekin Chheda <kekincchheda@gmail.com>
2020-08-07 22:05:14 +0530

    Changes in unit testing

---
+++ spec/test.spec.js
@@ -0,0 +1,8 @@
+// import { describe } from "mocha";
+
+describe("Begin",()=>{
+    it("should pass the test case", (done)=>{
+        expect(true).toBeTruthy();
+        done();
+    })
+})
--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ request = require('supertest');
 
 describe('GET /', ()=>{
     it('should respond with Hello to node-express world!',(done)=>{
-        request(app).get('/').expect('Hello! This is massive change.',done);
+        // AuthenticatorAssertionResponse()
     });
 });
