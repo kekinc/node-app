89b90c3846f679c94da69837497c0bc4bc5fc1be
kekinc <kekincchheda@gmail.com>
2020-08-07 23:53:23 +0530

    Adding more test cases to spec file

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -4,5 +4,10 @@ describe("Begin",()=>{
     it("should pass the test case.", (done)=>{
         expect(true).toBeTruthy();
         done();
+    });
+    
+    it("should pass the second test case.", (done)=>{
+        expect(false).toBeFalsy();
+        done();
     })
 })
