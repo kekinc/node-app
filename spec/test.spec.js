5ee33834d1ed6cc7d7c88ec135f74a9c58667e56
Kekin Chheda <kekincchheda@gmail.com>
2020-08-07 22:17:41 +0530

    Changes in spec file

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,7 +1,7 @@
 // import { describe } from "mocha";
 
 describe("Begin",()=>{
-    it("should pass the test case", (done)=>{
+    it("should pass the test case.", (done)=>{
         expect(true).toBeTruthy();
         done();
     })
